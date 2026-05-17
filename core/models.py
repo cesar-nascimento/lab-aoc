4ed0d95a62438ffe3c12abae356a9f286cd67f7d
César Rocha <>
2026-05-17 16:24:53 -0300

    start project

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Exchange(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
---
+++ lab_aoc_django_backend/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for lab_aoc_django_backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lab_aoc_django_backend.settings")
+
+application = get_wsgi_application()
+
+app = application
