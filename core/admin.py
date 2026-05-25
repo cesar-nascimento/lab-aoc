63b8398bfa80164f38cd1caae102773d42b8a3f1
César Rocha <>
2026-05-25 12:24:07 -0300

    add models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Exchange
+from .models import Asset, Exchange, Market, Ticker
 
 admin.site.register(Exchange)
+admin.site.register(Asset)
+admin.site.register(Market)
+admin.site.register(Ticker)
